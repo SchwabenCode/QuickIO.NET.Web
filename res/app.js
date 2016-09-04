fa2122780e727ba2f0c4f04547b59c42b7bdf4cd
Benjamin Abt <mail@benjamin-abt.com>
2016-09-04 17:27:28 +0200

    Moved to jekyll

---
+++ res/app.js
@@ -0,0 +1,47 @@
+﻿angular.module('QuickIO.Modules.ExampleModule', ['ui.router'])
+.config(
+  ['$stateProvider', '$urlRouterProvider',
+      function ($stateProvider, $urlRouterProvider) {
+          $stateProvider
+              .state('Examples', {
+                  url: '/Examples',
+                  abstract: true,
+                  templateUrl: 'views/Examples/Examples.html'
+              })
+              .state('Examples.Home', {
+                  url: '',
+                  templateUrl: 'views/Examples/Home.html'
+              })
+              .state('Examples.Jobs', {
+                  url: '/Jobs',
+                  templateUrl: 'views/Examples/Jobs.html'
+              })
+              .state('Examples.Observer', {
+                  url: '/Observer',
+                  templateUrl: 'views/Examples/Observer.html'
+              })
+              .state('Examples.Services', {
+                  url: '/Services',
+                  templateUrl: 'views/Examples/Services.html'
+              });
+
+      }]
+);
+
+angular.module('QuickIO', ['QuickIO.Modules.ExampleModule', 'ui.router', 'ngAnimate'])
+.run(['$rootScope', '$state', '$stateParams',
+    function ($rootScope, $state, $stateParams) {
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+    }])
+.config(['$stateProvider', '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider
+            .otherwise('/');
+        $stateProvider
+            .state("Home", {
+                url: "/",
+                templateUrl: 'views/Home.html',
+            });
+    }]
+);
